6a6972a25d869c74d12d7766df0841933220c1ee
giacomobg <pepperroni@hotmail.co.uk>
2020-10-16 16:32:26 +0100

    Add js

---
+++ how-to-template/template/config.js
@@ -0,0 +1,18 @@
+var dvc = {
+  // essential is for things that need changing
+  essential: {
+    title: "This is the title",
+    source: "This is the source"
+  },
+  // optional is for the rest
+  optional: {
+    breakpoint_sm: 450, // mobile
+    breakpoint_md: 600, // tablet
+    margin: {
+      top: 10,
+      right: 10,
+      bottom: 10,
+      left: 10
+    }
+  }
+}
---
+++ how-to-template/template/script.js
@@ -0,0 +1,34 @@
+var graphic = d3.select('#graphic');
+var pymChild = null;
+
+console.log('Running script.js!')
+
+function drawGraphic() {
+  console.log('Running drawGraphic')
+  graphic.selectAll('*').remove();
+}
+
+//check whether browser can cope with svg
+if (Modernizr.svg) {
+  //load chart data
+  console.log('get csv')
+  d3.csv(dvc.essential.graphic_data_url, function(error, data) {
+
+    graphic_data = data;
+    console.log(graphic_data)
+    // graphic_data.forEach(function(d) {
+    //   d.date = d3.timeParse(dvc.essential.dateFormat)(d.date);
+    // });
+
+    //use pym to create iframed chart dependent on specified variables
+    pymChild = new pym.Child({ renderCallback: drawGraphic});
+  });
+
+
+} else {
+  //use pym to create iframe containing fallback image (which is set as default)
+  pymChild = new pym.Child();
+  if (pymChild) {
+    pymChild.sendHeight();
+  }
+}
